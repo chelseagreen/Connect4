e57b27d5acb3f97e1d90cd74e9cbccb42e618458
Chelsea Green <greenchelc@gmail.com>
2016-12-06 22:34:27 -0500

    fix tests

--- app/game.component.spec.ts
+++ app/game.component.spec.ts
@@ -1,13 +1,11 @@
 import { GameComponent } from './game.component';
-import { WinnerService } from "./winner.service";
-import { inject, async, TestBed } from "@angular/core/testing";
+import { async, TestBed } from "@angular/core/testing";
 import { HttpModule } from "@angular/http";
 
 describe("connect 4", () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      providers: [WinnerService],
       imports: [HttpModule]
     });
     TestBed.compileComponents();
@@ -18,90 +16,90 @@ describe("connect 4", () => {
     expect(connect4.stateService.state).toEqual(connect4.stateService.playerOneTurnState);
   });
 
-  it("should change state when player one plays a tile", inject([WinnerService], () => {
+  it("should change state when player one plays a tile", () => {
     let connect4 = new GameComponent();
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(1);
     expect(connect4.stateService.state).toEqual(connect4.stateService.playerTwoTurnState);
-  }));
+  });
 
-  it("should switch player states back and forth", inject([WinnerService], () => {
+  it("should switch player states back and forth", () => {
     let connect4 = new GameComponent();
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(1);
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(1);
     expect(connect4.stateService.state).toEqual(connect4.stateService.playerOneTurnState);
-  }));
+  });
 
-  it("should add to both player selected tile lists", inject([WinnerService], () => {
+  it("should add to both player selected tile lists", () => {
     let connect4 = new GameComponent();
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(1);
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(2);
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(3);
     expect(connect4.stateService.playerOneSelectedTiles).toEqual([[1,0], [3,0]]);
     expect(connect4.stateService.playerTwoSelectedTiles).toEqual([[2,0]]);
-  }));
+  });
 
-  it("should increase y value of tile each time tile is added to selected tile list", inject([WinnerService], () => {
+  it("should increase y value of tile each time tile is added to selected tile list", () => {
     let connect4 = new GameComponent();
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(1);
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(1);
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(1);
     expect(connect4.stateService.playerOneSelectedTiles).toEqual([[1,0], [1,2]]);
     expect(connect4.stateService.playerTwoSelectedTiles).toEqual([[1,1]]);
-  }));
+  });
 
-  it('should show player one isWinner by consecutive y vals, equal x vals', inject([WinnerService], () => {
+  it('should show player one isWinner by consecutive y vals, equal x vals', () => {
     let connect4 = new GameComponent();
     connect4.stateService.playerOneSelectedTiles = [[3,0], [4,0], [2,0], [3,1], [3,2]];
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(3);
     expect(connect4.stateService.state).toEqual(connect4.stateService.playerOneWinnerState);
-  }));
+  });
 
-  it('should show player one isWinner by consecutive x vals, equal y vals', inject([WinnerService], () => {
+  it('should show player one isWinner by consecutive x vals, equal y vals', () => {
     let connect4 = new GameComponent();
     connect4.stateService.playerOneSelectedTiles = [[3,0], [4,0], [2,0], [3,1], [3,2]];
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(5);
     expect(connect4.stateService.state).toEqual(connect4.stateService.playerOneWinnerState);
-  }));
+  });
 
-  it('should not show isWinner if filled cells are filled with different player values', inject([WinnerService], () => {
+  it('should not show isWinner if filled cells are filled with different player values', () => {
     let connect4 = new GameComponent();
     connect4.stateService.playerOneSelectedTiles = [[3,0], [4,0], [2,0], [3,2]];
     connect4.stateService.playerTwoSelectedTiles = [[3,1], [5,0], [0,0], [1,0]];
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(3);
     expect(connect4.stateService.state).toEqual(connect4.stateService.playerTwoTurnState);
-  }));
+  });
 
-  it('should let player 2 win if 4 y values in a row', inject([WinnerService], () => {
+  it('should let player 2 win if 4 y values in a row', () => {
     let connect4 = new GameComponent();
     connect4.stateService.playerOneSelectedTiles = [[1,0], [3,0], [3,1], [3,2]];
     connect4.stateService.playerTwoSelectedTiles = [[2,0], [2,1], [2,2]];
     connect4.stateService.state = connect4.stateService.playerTwoTurnState;
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(2);
     expect(connect4.stateService.state).toEqual(connect4.stateService.playerTwoWinnerState);
-  }));
+  });
 
-  it('should show a diagonal upward isWinner', inject([WinnerService], () => {
+  it('should show a diagonal upward isWinner', () => {
     let connect4 = new GameComponent();
     connect4.stateService.playerOneSelectedTiles = [[0,0], [1,1], [2,2], [3,0], [3,2]];
     connect4.stateService.playerTwoSelectedTiles = [[1,0], [2,0], [2,1], [3,1], [4,0]];
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(3);
     expect(connect4.stateService.state).toEqual(connect4.stateService.playerOneWinnerState);
-  }));
+  });
 
-  it('should not show a diagonal when not four in a row', inject([WinnerService], () => {
+  it('should not show a diagonal when not four in a row', () => {
     let connect4 = new GameComponent();
     connect4.stateService.playerOneSelectedTiles = [[6,0], [5,1], [5,2], [4,1], [3,0]];
     connect4.stateService.playerTwoSelectedTiles = [[6,1], [5,0], [4,0], [4,2]];
     connect4.stateService.state = connect4.stateService.playerTwoTurnState;
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(3);
     expect(connect4.stateService.state).toEqual(connect4.stateService.playerOneTurnState);
-  }));
+  });
 
-  it('should show a diagonal downward isWinner', inject([WinnerService], () => {
+  it('should show a diagonal downward isWinner', () => {
     let connect4 = new GameComponent();
     connect4.stateService.playerOneSelectedTiles = [[5,0], [4,1], [3,2], [2,0], [2,2]];
     connect4.stateService.playerTwoSelectedTiles = [[4,0], [3,0], [3,1], [2,1], [1,0]];
     connect4.stateService.checkIfRowCanBePlayedAndSelectTile(2);
     expect(connect4.stateService.state).toEqual(connect4.stateService.playerOneWinnerState);
-  }));
+  });
 });
